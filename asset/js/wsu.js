f663e9a373ae97a4cbb0b9d8634d22005a89ee71
lionick <lionick85@hotmail.com>
2022-11-07 12:26:18 +0000

    fix various issues

--- asset/js/wsu.js
+++ asset/js/wsu.js
@@ -1,6 +1,16 @@
 (function($) {
-  $(document).ready(function() {
+  $(window).on('load', function() {
+    // bug fix when we search from home page and then we want to go back clicking back button's browser, show the value(s) selected before.
+    if($("select[name='facet[dcterms_subject_ss][]']").val()!= null){
+      $("select[name='facet[dcterms_subject_ss][]']").parent().find(".chosen-single span").eq(0).text($("select[name='facet[dcterms_subject_ss][]']").val())
+    }
+    if($("select[name='facet[dcterms_educationLevel_ss][]']").val()!= null){
+      $("select[name='facet[dcterms_educationLevel_ss][]']").parent().find(".chosen-single span").eq(1).text($("select[name='facet[dcterms_educationLevel_ss][]']").val())
+    }
+   });
 
+  $(document).ready(function() {
+    
     // Add loading animation when a form is submitted, when any item with a "spin" class is clicked,
     // or on any button or anchor tag lacking the .nospin class.
     $("input[type='submit'], button:not(.nospin), .spin").on('click', function() {
@@ -14,5 +24,7 @@
       }
 
     });
+    
+    $("#contact-us").attr("action", "#contact");
   });
 })(jQuery)
